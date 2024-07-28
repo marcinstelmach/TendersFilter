d201835205d225d29683e5d11463d5f4b1b99fdb
Marcin Stelmach <marcin.stelmach.ext@wsa.com>
2024-07-28 16:16:04 +0200

    Validate request

---
+++ FilterTenders.Api/ViewModels/TendersRequestFilter.cs
@@ -0,0 +1,16 @@
+﻿namespace FilterTenders.Api.ViewModels;
+
+public class TendersRequestFilter : IEndpointFilter
+{
+    public async ValueTask<object?> InvokeAsync(EndpointFilterInvocationContext context, EndpointFilterDelegate next)
+    {
+        var request = context.GetArgument<GetTendersRequest>(0);
+
+        if (request.OrderType is not null && request.OrderBy is null)
+        {
+            return Results.Problem("OrderType parameter require OrderBy parameter to be present", statusCode: 400);
+        }
+
+        return await next(context);
+    }
+}
